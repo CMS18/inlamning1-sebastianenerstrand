eaaa3d0383d7497bfaa3b1801f2cdf73d77f5f5e
Schlebbe <sebastian@temtron.se>
2019-11-11 15:43:03 +0100

    Added constructor injection and base values for account and customer. Display all customers and accounts in view.

---
+++ Inlämning1/Models/BankRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inlämning1.WebUI.Models
+{
+    public class BankRepository
+    {
+        public List<Customer> Customers { get; set; } = new List<Customer>();
+        public List<Account> Accounts { get; set; } = new List<Account>();
+
+        public BankRepository()
+        {
+            Customers.Add(new Customer { CustomerID = 1, CustomerName = "Lisa" });
+            Customers.Add(new Customer { CustomerID = 2, CustomerName = "Fredrik" });
+            Customers.Add(new Customer { CustomerID = 3, CustomerName = "Adam" });
+
+            Accounts.Add(new Account { AccountID = 1, CustomerID = 2, Balance = 50.22M });
+            Accounts.Add(new Account { AccountID = 2, CustomerID = 1, Balance = 7224.91M });
+            Accounts.Add(new Account { AccountID = 3, CustomerID = 3, Balance = 6123.84M });
+        }
+
+        public void Transfer()
+        {
+
+        }
+
+        public void Withdraw()
+        {
+
+        }
+
+        public void Deposit()
+        {
+
+        }
+    }
+}
---
+++ Inlämning1/Models/ViewModels/CustomerAccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inlämning1.WebUI.Models.ViewModels
+{
+    public class CustomerAccountViewModel
+    {
+        public List<Customer> Customers { get; set; }
+        public List<Account> Accounts { get; set; }
+    }
+}
