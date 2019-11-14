db6d667d5ad74f0a8dc577628168bc2d55261cf3
Linnea <linnea.eriksson91@hotmail.com>
2019-11-14 12:16:18 +0100

    Added functionality for transfers

--- Inlämning1.Tests/UnitTest1.cs
+++ Inlämning1.Tests/UnitTest1.cs
@@ -42,5 +42,33 @@ namespace Inlämning1.Tests
             //Assert & Act
             Assert.Throws<ArgumentOutOfRangeException>(() => repo.Withdraw(withdrawAmount, account));
         }
+
+        [Fact]
+        public void Transfer()
+        {
+            //Arrange
+            var amountToTransfer = 100m;
+            var accountFrom = new Account { Balance = 225.5m, AccountID = 1};
+            var accountTo = new Account { Balance = 10m, AccountID = 2};
+
+            //Assert
+            accountFrom.Transfer(accountFrom, accountTo, amountToTransfer);
+
+            //Assert
+            Assert.Equal(225.5m-amountToTransfer, accountFrom.Balance);
+            Assert.Equal(10m + amountToTransfer, accountTo.Balance);
+        }
+
+        [Fact]
+        public void TransferAmountOVerZero()
+        {
+            //Arrange
+            var amountToTransfer = 101m;
+            var accountFrom = new Account { Balance = 100m, AccountID = 1 };
+            var accountTo = new Account { Balance = 10m, AccountID = 2 };
+
+            //Act and assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => accountFrom.Transfer(accountFrom, accountTo, amountToTransfer));
+        }
     }
 }
